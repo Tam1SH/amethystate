d7e4e1857b2f4c26818dfe295fc87b83aa616200
Tam1SH <ignatternyev54@gmail.com>
2026-06-14 23:32:58 +0300

    fix path resolve

--- landing/src/utils/asset.ts
+++ landing/src/utils/asset.ts
@@ -1,2 +1,2 @@
 const base = import.meta.env.BASE_URL.replace(/\/$/, '');
-export const asset = (path: string) => `${base}${path}`;
+export const asset = (path: string) => `${base}/${path.replace(/^\//, '')}`;
